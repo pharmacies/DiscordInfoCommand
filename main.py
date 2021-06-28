f141c17ef3c766e8b144af41026da41c0cd5df6f
Agarz <86589008+pharmacies@users.noreply.github.com>
2021-06-28 02:46:47 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix="EnterYourPrefix", case_insensitive=True, intents=intents)
+
+
+
+@client.event
+async def on_ready():
+  await client.change_presence(status = discord.Status.online, activity = discord.Game(".help"))
+  print("Bot is ready")
+  
+  
+  
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+        
+
+token = "Your bot Token"        
+
+client.run(token)
